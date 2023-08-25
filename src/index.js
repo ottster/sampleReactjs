328f8758712ab7753b5a77d90dbfa71009e586f2
ottster <omaiwat@gmail.com>
2023-08-24 19:55:45 -0500

    update reactDom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // need to render the App component and attach it to the root DOM element of your HTML file. Here's the code you would typically have in your index.js:
 
 import React from 'react';
-import { createRoot } from 'react-dom'; // Import createRoot
+// import { createRoot } from 'react-dom'; // Import createRoot
+import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.css';
 
